e1859fa0375af90f5deee034d319e6683661017a
Louis-Mozart <louismozart.teyou@aims.ac.rw>
2026-01-02 17:38:53 +0100

    Add initial file structure with imports and documentation

---
+++ ml_convergence_comparison.py
@@ -0,0 +1,7 @@
+"""
+Simple Machine Learning Model: Linear Regression
+Comparing Fixed Learning Rate vs Adaptive Learning Rate (Barzilai-Borwein)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
