e04e5200572e49f77d1f78dc2db6b6730815530c
Louis-Mozart <louismozart.teyou@aims.ac.rw>
2026-01-06 19:09:39 +0100

    Initialize polynomial regression file with imports and documentation

---
+++ polynomial_convergence_comparison.py
@@ -0,0 +1,7 @@
+"""
+Polynomial Regression: 2nd Degree Polynomial Fitting
+Comparing Fixed Learning Rate vs Adaptive Learning Rate (Barzilai-Borwein)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
