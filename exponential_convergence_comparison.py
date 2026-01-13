f39bde767632b172ec4b31ecd75e1e75aa8d98c5
dice <louismozart.teyou@aims.ac.rw>
2026-01-13 21:31:15 +0100

    Add fixed learning rate gradient descent implementation

--- exponential_convergence_comparison.py
+++ exponential_convergence_comparison.py
@@ -16,3 +16,19 @@ def compute_function(x):
 def compute_gradient(x):
     """Compute gradient df/dx = e^x + 1."""
     return np.exp(x) + 1
+
+
+def gradient_descent_fixed(x_init=0.0, learning_rate=0.01, n_iterations=100):
+    """Gradient Descent with Fixed Learning Rate."""
+    x = x_init
+    x_history = [x]
+    f_history = [compute_function(x)]
+    
+    for i in range(n_iterations):
+        grad = compute_gradient(x)
+        x = x - learning_rate * grad
+        
+        x_history.append(x)
+        f_history.append(compute_function(x))
+    
+    return x, x_history, f_history
